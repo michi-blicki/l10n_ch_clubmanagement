4b355dc6e4334fb729d0080853068a4bbb929968
Michi Blickenstorfer <michi@blicki.ch>
2026-01-11 11:02:12 +0100

    0.4a: align with clubmanagement main module

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@ This is a small Addon, that enhances the clubmanagement suite, available at http
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Association',
-    'version': '18.0.0.3.0',
+    'version': '18.0.0.4.0',
     'application': False,
     'auto_install': False,
     'installable': True,
